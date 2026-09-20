d7918f3ade9d27afef2b8239b252465b50783373
Alectardy98 <61422584+Alectardy98@users.noreply.github.com>
2026-09-20 17:49:39 -0400

    fix

--- keyboards/cipulot/ec_prox/ansi_iso/config.h
+++ keyboards/cipulot/ec_prox/ansi_iso/config.h
@@ -16,8 +16,6 @@
 
 #pragma once
 
-#define MATRIX_ROWS 5
-#define MATRIX_COLS 15
 
 #define UNUSED_POSITIONS_LIST { {1, 14}, {2, 14}, {3, 12}, {4, 0}, {4, 3}, {4, 4}, {4, 5}, {4, 7}, {4, 8}, {4, 11}, {4, 12}, {4, 13}, {4, 14} }
 
